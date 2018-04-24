6cc603a2d6b3678ad7b65e897de43f81dfe78fae
marudy <maroudasa@gmail.com>
2018-04-24 12:03:24 +0300

    Add listenOrientationChange method that listens orientation change and triggers a screen rerender.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // packages
 import { Dimensions, PixelRatio } from 'react-native';
 
-// Retrieve screen's width once 
+// Retrieve screen's width once
 const screenWidth = Dimensions.get('window').width;
 
 // Retrieve screen's height once
 const screenHeight = Dimensions.get('window').height;
 
 /**
- * Converts provided width percentage to independent pixel (dp).  
+ * Converts provided width percentage to independent pixel (dp).
  * @param  {string} widthPercent The percentage of screen's width that UI element should cover
  *                               along with the percentage symbol (%).
  * @return {number}              The calculated dp depending on current device's screen width.
@@ -18,7 +18,7 @@ const widthPercentageToDP = widthPercent => {
   const elemWidth = parseFloat(widthPercent);
 
   // Use PixelRatio.roundToNearestPixel method in order to round the layout
-  // size (dp) to the nearest one that correspons to an integer number of pixels. 
+  // size (dp) to the nearest one that correspons to an integer number of pixels.
   return PixelRatio.roundToNearestPixel(screenWidth * elemWidth / 100);
 };
 
@@ -37,7 +37,31 @@ const heightPercentageToDP = heightPercent => {
   return PixelRatio.roundToNearestPixel(screenHeight * elemHeight / 100);
 };
 
+/**
+ * Event listener function that detects orientation change (every time it occurs) and triggers 
+ * screen rerendering. It does that, by changing the state of the screen where the function is
+ * called. State changing occurs for a new state variable with the name 'orientation' that will
+ * always hold the current value of the orientation after the 1st orientation change.
+ * Invoke it inside the screen's constructor or in componentDidMount lifecycle method and remove
+ * it in componentWillUnMount lifecycle method.
+ * @param {object} that Screen's class component this variable. The function needs it to
+ *                      invoke setState method and trigger screen rerender (this.setState()).
+ */
+const listenOrientationChange = (that) => {
+  Dimensions.addEventListener('change', newDimensions => {
+    // Retrieve and save new dimensions
+    screenWidth = newDimensions.window.width;
+    screenHeight = newDimensions.window.height;
+
+    // Trigger screen's rerender with a state update of the orientation variable
+    that.setState({
+      orientation: screenWidth < screenHeight ? 'portrait' : 'landscape'
+    });
+  });
+};
+
 export {
   widthPercentageToDP,
-  heightPercentageToDP
+  heightPercentageToDP,
+  listenOrientationChange 
 };
