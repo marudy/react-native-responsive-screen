2894e4c620ad95a346b53c8ad6cfd680e1da1375
marudy <maroudasa@gmail.com>
2018-05-02 22:18:59 +0300

    Add example on how to use react-native-responsive-package with styled components

--- examples/responsive-screen-styled-components/App.js
+++ examples/responsive-screen-styled-components/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+// packages
+import * as React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { widthPercentageToDP, heightPercentageToDP } from 'react-native-responsive-screen';
+import styled from 'styled-components';
+ 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container>
+        <ResponsiveBox>
+          <DemoText>This box is always of 84.5% width and 17% height.</DemoText>
+          <DemoText>Test it by running this example repo in phones/
+            emulators with screens of various dimensions and pixel per inch (ppi).</DemoText>
+        </ResponsiveBox>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: gray;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ResponsiveBox = styled.View`
+  width: ${widthPercentageToDP('84.5%')};
+  height: ${heightPercentageToDP('17%')};
+  border-width: 2;
+  border-color: orange;
+  flex-direction: column;
+  justify-content: space-around; 
+`;
+
+const DemoText = styled.Text`
+  color: white;
+`;
