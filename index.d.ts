21a72653a81738dd0d6addf6481b85e7e4dbaef5
Zacchari CARDE <zcarde@student.42.fr>
2019-03-28 12:02:54 +0100

    Fix variable name

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare module 'react-native-responsive-screen' {
   import { Component } from 'react';
 
   export function widthPercentageToDP(widthPercent: string | number): number;
-  export function heightPercentageToDP(widthPercent: string | number): number;
+  export function heightPercentageToDP(heightPercent: string | number): number;
   export function listenOrientationChange(screenClassComponent: Component<any, any>): void;
   export function removeOrientationListener(): void;
 }
