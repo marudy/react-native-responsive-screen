996b66e2ab3b214e29c8bcb0f840c2a2d3020c7c
gregfenton <greg.fenton@gmail.com>
2020-08-07 15:05:40 -0400

    listenOrientationChange to support hooks
    Encapsulate the changes inspired by https://github.com/marudy/react-native-responsive-screen/pull/70

--- index.js
+++ index.js
@@ -38,24 +38,37 @@ const heightPercentageToDP = heightPercent => {
 };
 
 /**
- * Event listener function that detects orientation change (every time it occurs) and triggers 
+ * Event listener function that detects orientation change (every time it occurs) and triggers
  * screen rerendering. It does that, by changing the state of the screen where the function is
  * called. State changing occurs for a new state variable with the name 'orientation' that will
  * always hold the current value of the orientation after the 1st orientation change.
  * Invoke it inside the screen's constructor or in componentDidMount lifecycle method.
- * @param {object} that Screen's class component this variable. The function needs it to
- *                      invoke setState method and trigger screen rerender (this.setState()).
+ * @param {object} classComponent This Screen's class component this variable.
+ *                                listenOrientationChange() needs it to invoke the class's setState
+ *                                (this.setState()) method and trigger screen rerender.
+ * @param {object} setStateHook A set-state function, typically from useState().
+ *                              listenOrientationChange() calls it to set the state value and
+ *                              trigger screen rerender.
+ * @throws {Error} If neither or both of the params are set.  Must only set ONE of them.
  */
-const listenOrientationChange = that => {
+const listenOrientationChange = ({classComponentThis = null, setStateHook = null}) => {
   Dimensions.addEventListener('change', newDimensions => {
     // Retrieve and save new dimensions
     screenWidth = newDimensions.window.width;
     screenHeight = newDimensions.window.height;
 
-    // Trigger screen's rerender with a state update of the orientation variable
-    that.setState({
+    let orientation = {
       orientation: screenWidth < screenHeight ? 'portrait' : 'landscape'
-    });
+    };
+
+    // Trigger screen's rerender with a state update of the orientation variable
+    if (classComponentThis && ! setStateHook) {
+      classComponentThis.setState(orientation);
+    } else if (setStateHook && !classComponentThis) {
+      setStateHook(orientation);
+    } else {
+      throw new Error("Must set only ONE of classComponentThis or setStateHook");
+    }
   });
 };
 
